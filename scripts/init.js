35d21a249ca35354cdd8d6aa21086e0adfd2fd7d
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-09 19:59:23 +0900

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -48,6 +48,7 @@ const hpring = extend(Packages.arc.scene.style.Drawable, {
   draw(x, y, w, h){
     var hp = Vars.player.health()/Vars.player.maxHealth();
     var scl = this.hpscl(hp);
+    if(hp>0.99999) return;
     if(Mathf.equal(hp, scl) && hp < 0.9999) this.lastframe = Time.time();
     var a = (60 - (Time.time() - this.lastframe))/60;
     if(a <= 0) return;
